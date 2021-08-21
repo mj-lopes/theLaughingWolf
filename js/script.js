6c1d79d606d762237a9a76bab9c8c5096ad181b8
Marcos Júnior <marcos_lopes_ribeiro_junior@hotmail.com>
2021-08-20 23:12:47 -0300

    Adicionado scroll suave para os links internos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 import SlideNav from "./slide.js";
 import animaSection from "./anima-section.js";
+import ScrollSuave from "./scroll-suave.js";
 
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 
 const anima = new animaSection("[data-animaScroll]");
 anima.init();
+
+const scrollToSection = new ScrollSuave('a[href^="#"]');
+scrollToSection.init();
